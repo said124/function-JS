4a60347c826723b48b9bb0fc7005a5f5eefc37ab
said124 <165512725+said124@users.noreply.github.com>
2025-11-11 23:40:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function dark() {
+    document.body.style.backgroundColor = 'black'
+    document.body.style.color = 'white'
+}
+
+function white() {
+    document.body.style.backgroundColor = '#C4B5A5'
+    document.body.style.color = 'black'
+}
